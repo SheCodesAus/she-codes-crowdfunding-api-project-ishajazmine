ab824f0899fa3f2609f0d7c43a16db11e34d34d4
ishajazmine <a.fredericksen@live.com.au>
2022-03-15 18:14:31 +0800

    150322 - pledge serializer and views
    150322 - pledge serializer and views

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -1,5 +1,17 @@
 from rest_framework import serializers
-from .models import Project
+from .models import Project, Pledge
+
+# pledge serializer
+class PledgeSerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    amount = serializers.IntegerField()
+    comment = serializers.CharField(max_length=200)
+    anonymous = serializers.BooleanField()
+    supporter = serializers.CharField(max_length=200)
+    project_id = serializers.IntegerField()
+    
+    def create(self, validated_data):
+        return Pledge.objects.create(**validated_data)
 
 
 class ProjectSerializer(serializers.Serializer):
@@ -14,3 +26,4 @@ class ProjectSerializer(serializers.Serializer):
 
     def create(self, validated_data):
         return Project.objects.create(**validated_data)
+
